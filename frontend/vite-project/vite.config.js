9ee515a4dbeac59543569363e9f66d0675fd0ad1
harikanartu <harikanartu6@gmail.com>
2025-12-21 12:05:17 +0530

    Deply react build to Github Pages

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/intership-project/',
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8081/api",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+})
